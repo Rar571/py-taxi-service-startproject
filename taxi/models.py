0ed47b780cc8fca79eb264cccc4811f095e25a56
Ruslan <stadnikruslan168@gmail.com>
2026-02-21 20:43:30 +0200

    feat: add taxi app models, admin and migrations

--- taxi/models.py
+++ taxi/models.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import AbstractUser
 
 from django.db import models
 
+from django.conf import settings
+
 
 class Manufacturer(models.Model):
     name = models.CharField(max_length=63, unique=True)
@@ -10,8 +12,8 @@ class Manufacturer(models.Model):
 
 class Car(models.Model):
     model = models.CharField(max_length=63)
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-    drivers = models.ManyToManyField("taxi.Driver")
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name = "cars")
+    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="cars")
 
 
 class Driver(AbstractUser):
